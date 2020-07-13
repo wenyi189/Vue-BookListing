bb3383291651572d6c2b10e01642fd048eec865c
Yi Wen <wenyi189@gmail.com>
2020-07-12 23:07:37 -0700

    Completed module

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello-world></hello-world>
+    <!-- <hello-world></hello-world> -->
+    <book-list></book-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import BookList from './components/BookList';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    BookList,
   },
 };
 </script>
